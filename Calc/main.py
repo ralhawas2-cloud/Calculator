81caeed6849370f64ebc83d3d98cbe2d4bac6e8a
ralhawas2-cloud <ralhawas2@gmail.com>
2026-08-28 23:44:57 +0300

    Add files via upload

---
+++ Calc/main.py
@@ -0,0 +1,17 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+i = int(input())
+j = int(input())
+op = input()
+if op == "+":
+    print(i + j)
+elif op == "-":
+    print(i - j)
+elif op == "*":
+    print(i * j)
+elif op == "/":
+    print( i / j)
+
